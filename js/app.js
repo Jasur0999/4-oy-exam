b46e8b1c27b43a964bab1d556afe6975eb560018
Jasur0999 <leaderjasur@gmail.com>
2024-02-19 21:55:24 +0500

    sfbsdgbe

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// const btn1 = document.querySelector("#btn1");
+// const btn2 = document.querySelector("#btn2");
+// const menuList = document.querySelector(".menu-navbar");
+
+// btn1.addEventListener("click", (e) => {
+//   menuList.style = "transform: translate(0)";
+//   document.body.style = "overflow: hidden";
+// });
+
+// btn2.addEventListener("click", (e) => {
+//   menuList.style = "transform: translate(-100%)";
+// });
+
+
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", function () {
+    document.body.classList.toggle("menu-opened")
+});
